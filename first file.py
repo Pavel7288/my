f77e1da6e260d3e16afd7246c843f0d397ce0c4a
Pavel <bublik7288@gmail.com>
2025-01-08 18:30:30 +0300

    gff

--- first file.py
+++ first file.py
@@ -1,8 +1,6 @@
-print("hello")
-def i (p):
-    print(p)
-print('lpop')
-print('mod')
 print('lplpdffd')
 print("bye")
 print("i now")
+l = [1, 2, 3, 54, 56, 6, 7, 4]
+j = [i for i in l if i > 4]
+print(j)
