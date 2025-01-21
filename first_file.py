317cddb93b1af9e5d7ed7a7febcf1e7cbcc1e797
Pavel <bublik7288@gmail.com>
2025-01-21 19:21:49 +0300

    расставление принтов

--- first_file.py
+++ first_file.py
@@ -1,10 +1,10 @@
-def sum_all_numbers(*,item):
-     return sum(int(i) ** 2 for i in str(item))
-
+def sum_all_numbers(*, item):
+    return sum(int(i) ** 2 for i in str(item))
 
 
 def doing(numb: int, counter: int = 8) -> bool:
     for _ in range(counter):
+        print(sum_all_numbers(item=numb))
         numb = sum_all_numbers(item=numb)
         if numb == 1:
             return True
@@ -13,6 +13,7 @@ def doing(numb: int, counter: int = 8) -> bool:
 
 try:
     number = int(input('введите число: '))
+
     if doing(sum_all_numbers(item=number)):
         print('счастливое')
     else:
