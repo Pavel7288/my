f72a0fa2a285505b4e360b819c196baea6298363
Pavel <bublik7288@gmail.com>
2025-02-20 09:28:26 +0300

    finished

--- 1_file.py
+++ 1_file.py
@@ -2,6 +2,8 @@ import asyncio
 import sqlite3
 from datetime import datetime
 
+pending_orders = []
+
 
 class Dishes:
     def __init__(self, name, price, quantity=1):
@@ -10,7 +12,7 @@ class Dishes:
         self.price = price
 
     def __str__(self):
-        return f'{self.quantity} {self.name} - {self.price}$ '
+        return f'{self.quantity} x {self.name} - {self.price}$ '
 
 
 class Menu:
@@ -68,7 +70,6 @@ class Orders:
 class AllOrders:
     def __init__(self):
         self.current_all_orders = []
-        self.completed_orders = []
 
     def add(self, item):
         self.current_all_orders.append(item)
@@ -78,55 +79,73 @@ class AllOrders:
             if item.name_client.lower() == name.lower():
                 return item
 
-    async def changed_status_of_order(self, name,delay=60):
+    async def changed_status_of_order(self, name, delay=5):
         if name in self.current_all_orders:
-            name.ready = True
-            print(f'{name} is ready')
-            await asyncio.sleep(delay)
-            self.completed_orders.append(name)
-            self.current_all_orders.remove(name)
-            print(f'Order {name}- Was removed')
+            if not name.ready:
+                name.ready = True
+                print(f'{name} is ready')
+                await asyncio.sleep(delay)
+                self.current_all_orders.remove(name)
+                print(f'Order {name}- Was removed')
+                await insert_order(customer_name=name.name_client, price=name.full_cost_every_order(),
+                                   products=name.add_to_database())
+            else:
+                print('That name was changed status')
 
     def empty_list(self):
         if not self.current_all_orders:
             return True
 
-    def show_current_orders(self):
-        current_orders = ''.join([str(item) for item in self.current_all_orders])
-        return current_orders
-
-    def show_finished_orders(self):
-        sum_all_finished_orders = sum(list_orders.full_cost_every_order() for list_orders in self.completed_orders)
-        finished_orders = ''.join([str(item) for item in self.completed_orders])
-        return f'sum cost all orders - {sum_all_finished_orders}$, all finished orders - {finished_orders}'
-
     def __str__(self):
-        return (f'List current orders : {self.show_current_orders()}'
-                f'List finished orders : {self.show_finished_orders()}')
+        return ''.join([str(item) for item in self.current_all_orders])
 
 
 def get_next_order_counter():
     current_date = datetime.now().strftime('%Y-%m-%d')
-    with sqlite3.connect('/home/pavel/Documents/Пустой документ') as conn:
+    with sqlite3.connect('/home/pavel/Documents/my_project') as conn:
         cursor = conn.cursor()
         cursor.execute('SELECT COUNT(*) FROM finished_orders WHERE DATE(datetime) = ?', (current_date,))
         count = cursor.fetchone()[0]
         return count + 1
 
 
-def insert_order(*, customer_name, price, products):
-    order_counter = get_next_order_counter()
-    current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+async def insert_order(customer_name, price, products, order_counter=False, current_datetime=False):
+    if not order_counter and not current_datetime:
+        order_counter = get_next_order_counter()
+        current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    try:
+        with sqlite3.connect("/home/pavel/Documents/my_project") as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                    INSERT INTO finished_orders (datetime, order_counter, name_client, price, products)  
+                    VALUES (?, ?, ?, ?, ?)
+                """, (current_datetime, order_counter, customer_name, price, products))
+            conn.commit()
+            print(f'Order №{order_counter} added to database')
+            return True
 
-    with sqlite3.connect("/home/pavel/Documents/Пустой документ") as conn:
-        cursor = conn.cursor()
-        cursor.execute("""
-            INSERT INTO finished_orders (datetime, order_counter, name_client, price, products)  
-            VALUES (?, ?, ?, ?, ?)
-        """, (current_datetime, order_counter, customer_name, price, products))
-        conn.commit()
+    except (sqlite3.OperationalError, sqlite3.DatabaseError) as e:
+        print(f"⚠️ Ошибка при записи заказа {order_counter} : {e}")
+        products_ = current_datetime, order_counter, customer_name, price, products
+        if set(products_) not in pending_orders:
+            pending_orders.append(set(products_))
+            print(f"Order №{order_counter} added to bad database.")
+        else:
+            print('Order already exist in bad database')
+        print(pending_orders)
 
-    print(f"Заказ №{order_counter} добавлен в БД.")
+
+async def retry_pending_orders(interval=5):
+    """ Периодически повторяет попытки записи заказов из очереди в БД. """
+    while True:
+        if pending_orders:
+            print(f"🔄 Повторная попытка отправки {len(pending_orders)} заказов...")
+            for order in pending_orders.copy():
+                success = await insert_order(*order)
+                if success:
+                    pending_orders.remove(order)
+                    print('записан')
+        await asyncio.sleep(interval)
 
 
 coffe = Dishes(name='coffe', price=2)
@@ -149,56 +168,56 @@ Natasha.create_order(milk)
 all_orders = AllOrders()
 all_orders.add(Masha)
 all_orders.add(Natasha)
-print(all_orders.show_current_orders())
+print(all_orders)
 
-while True:
-    command = str(input('What you wish: '))
-    if command == 'add order':
-        name_visitor = str(input('Enter client: '))
-        print(f'Name of client is {name_visitor}')
-        name_client_1 = Orders(name_client=name_visitor)
-        while True:
-            name_of_order = str(input('Enter name of order: '))
-            if name_of_order == 'e':
-                break
-            elif menu.get_product(name_of_order):
-                ordered_dish = menu.get_product(name_of_order)
-                while True:
-                    try:
-                        quantity_of_product = int(input('enter quantity of product: '))
-                        if isinstance(quantity_of_product, int):
-                            print(f'quantity of product set {quantity_of_product}')
+
+async def main():
+    asyncio.create_task(retry_pending_orders())
+    while True:
+        command = await asyncio.to_thread(input, 'What you wish: ')
+        if command == 'add order':
+            name_visitor = await asyncio.to_thread(input, 'Enter client: ')
+            print(f'Name of client is {name_visitor}')
+            name_client_1 = Orders(name_client=name_visitor)
+            while True:
+                name_of_order = str(input('Enter name of order: '))
+                if name_of_order == 'end':
+                    break
+                elif menu.get_product(name_of_order):
+                    ordered_dish = menu.get_product(name_of_order)
+                    while True:
+                        try:
+                            quantity_of_product = int(await asyncio.to_thread(input, 'enter quantity of product: '))
+                            if isinstance(quantity_of_product, int):
+                                print(f'quantity of product set {quantity_of_product}')
+                                break
+                        except ValueError:
+                            quantity_of_product = 1
+                            print('quantity of product set 1')
                             break
-                    except ValueError:
-                        quantity_of_product = 1
-                        print('quantity of product set 1')
-                        break
-                name_client_1.create_order(ordered_dish, quantity_of_product)
-                print(name_client_1)
+                    name_client_1.create_order(ordered_dish, quantity_of_product)
+                    print(name_client_1)
+                else:
+                    print("That order doesn't exist")
+            all_orders.add(name_client_1)
+            print(f'Order - {name_client_1} Added')
+            print(all_orders)
+        elif command == 'ready':
+            while True:
+                print(all_orders)
+                name_that_remove = await asyncio.to_thread(input, 'Enter name that need remove: ')
+                item_that_remove = all_orders.get_item_all_orders(name_that_remove)
+                if item_that_remove:
+                    break
+                else:
+                    print("list of name doesn't have such name")
+            asyncio.create_task(all_orders.changed_status_of_order(item_that_remove))
+            await asyncio.sleep(0)
+            if not all_orders.empty_list():
+                print(all_orders)
             else:
-                print("That order doesn't exist")
-        all_orders.add(name_client_1)
-        print(f'Order - {name_client_1} added')
-        print(all_orders.show_current_orders())
-    elif command == 'ready':
-        while True:
-            print(all_orders.show_current_orders())
-            name_that_remove = str(input('Enter name that need remove: '))
-            item_that_remove = all_orders.get_item_all_orders(name_that_remove)
-            if item_that_remove:
-                break
-            else:
-                print("list of name doesn't have such name")
-        insert_order(customer_name=name_that_remove, price=item_that_remove.full_cost_every_order(),
-                     products=item_that_remove.add_to_database())
-        all_orders.changed_status_of_order(item_that_remove)
-        if not all_orders.empty_list():
-            print(all_orders.show_current_orders())
-        else:
-            print('List of order is empty')
-    elif command == 'finished':
-        print(all_orders.show_finished_orders())
-    # elif command == 'end of the working day':
-    # print(all_orders.show_finished_orders())
+                print('List of order is empty')
 
 
+if __name__ == '__main__':
+    asyncio.run(main())
